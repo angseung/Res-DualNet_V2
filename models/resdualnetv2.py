3866dbf1691fd08f2c1d4df6aef215266151c057
angseung <angseung@outlook.com>
2022-12-17 21:08:44 +0900

    Reverted config to test case #9

--- models/resdualnetv2.py
+++ models/resdualnetv2.py
@@ -53,7 +53,6 @@ class DWHT(nn.Module):
         if self.N < self.M:
             x = F.pad(x, (0, 0, 0, 0, 0, (self.M - self.N)), "constant", 0)
 
-        # for i in range(self.n):
         e = x[:, ::2, :, :]
         o = x[:, 1::2, :, :]
         x[:, : (self.M // 2), :, :] = e + o
